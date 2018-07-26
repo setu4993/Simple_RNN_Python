5eb9f1e90749690648986434fb436310b3921b89
Setu Shah <setu4993@yahoo.co.in>
2018-07-25 19:45:22 -0700

    Added descriptions to functions

--- rnn/file_io.py
+++ rnn/file_io.py
@@ -1,7 +1,8 @@
 import csv
-import numpy as np
 import pickle
 import logging
+import numpy as np
+
 
 INPUT_NODES = 3
 HIDDEN_NODES = 8
@@ -11,8 +12,8 @@ TOTAL_NETWORKS = 12
 
 def load_weights_from_csv(csv_location):
     """
-    Loads the weights from a CSV file into 4 objects. Not recommended. The CSV structure is complicated and prone to
-    errors. Instead use `np.ndarray`s of equivalent type.
+    Loads the weights from a CSV file into 4 objects. Not recommended for regular use. The CSV structure is complicated
+    and prone to errors. Instead use `np.ndarray`s of equivalent type.
 
     :param csv_location: Location of the CSV file on disk.
     :return: - wih: Weights for the neural network connecting the input-hidden layer. 3D array of type `np.ndarray` with
@@ -120,6 +121,23 @@ def save_weights_to_pickle_dump(dump_location, wih, whh, who, prh):
 
 
 def read_input_file(input_location):
+    """
+
+    :param input_location: Location to the input CSV file for prediction.
+    :return: - inputs: List of multiple `[day, temp, prec]` lists containing the real-world values of the input
+                       variables. Ranges do not correspond to maximum or minimum possible, but approximately to
+                        [- 2.5 standard deviations, + 2.5 standard deviation].
+                -- day: Integer value for day of the year. Range: [0-365]
+                -- temp: Integer value of maximum temperature for the day, as tenths of degrees Celsius. For 30.0C, the
+                         value of this variable should be 300. Range: [-100, 300]
+                -- prec: Integer value for precipitation (sum of rainfall and snowfall) of the day, in mm.
+                         Range: [0, 252]
+             - networks: List containing the index of networks these inputs should run on. Typically corresponds to the
+                        month of the year the prediction is for. The length of this list is the same as the length of
+                        input list. Range: [0, 11]
+             - targets: List containing target values for the given inputs. The length of this list is the same as the
+                        length of input list.
+    """
     input_file = open(input_location, 'r')
     input_lines = csv.reader(input_file)
     inputs = []
@@ -135,6 +153,15 @@ def read_input_file(input_location):
 
 
 def write_output_to_file(targets, predictions, output_location):
+    """
+    Writes the output to a CSV file.
+
+    :param targets: List containing target values for the given inputs. The length of this list is the same as the
+                    length of input list.
+    :param predictions: List containing predicted values for the given inputs. The length of this list is the same as
+                        the length of input list.
+    :param output_location: String containing the address on disk for the output file.
+    """
     output_file = open(output_location, 'w', newline='')
     output_writer = csv.DictWriter(output_file, fieldnames=['Target', 'Prediction', 'Error', 'Error Percentage'])
     output_writer.writeheader()
@@ -145,6 +172,9 @@ def write_output_to_file(targets, predictions, output_location):
 
 
 def log_to_console():
+    """
+    Initiates a console logger and sets the format. Recommended if not running through an IDE or a Jupyter Notebook.
+    """
     logger = logging.getLogger()
     logger.setLevel('DEBUG')
     consoleHandler = logging.StreamHandler()
