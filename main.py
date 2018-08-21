cb1148e35fc6b1ab58425a0abd1fe05a251324bb
Setu Shah <setu4993@yahoo.co.in>
2018-08-21 17:55:19 -0400

    Modified to use new class structure

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import os
-from rnn import *
+from rnn import rnn_predict, read_input_file, write_output_to_file
 
 DATA_LOCATION = os.getcwd()
 INPUT_FILE = os.path.join(DATA_LOCATION, 'input_files', 'water_6_17_reduced_2_cap_rs.csv')
 
 
 if __name__ == '__main__':
-    rnn = recurrent_neural_network(os.path.join(DATA_LOCATION, 'input_files', 'daily_rnn_retrained_16.pickle'), alpha=0.38, log=True)
+    predictor = rnn_predict(os.path.join(DATA_LOCATION, 'input_files', 'daily_rnn_retrained_16.pickle'), alpha=0.38, log=True)
     inputs, networks, targets = read_input_file(INPUT_FILE)
-    predictions = rnn.run_network(inputs, networks=networks, targets=targets, recal=True)
+    predictions = predictor.predict(inputs, networks=networks, targets=targets, recal=True)
     write_output_to_file(targets, predictions, os.path.join(DATA_LOCATION, 'predictions_2017.csv'))
-    rnn.save(os.path.join(DATA_LOCATION, 'input_files', 'daily_rnn_retrained_17.pickle'))
+    predictor.save(os.path.join(DATA_LOCATION, 'input_files', 'daily_rnn_retrained_17.pickle'))
